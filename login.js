35063a6c4230b38e43769b0a7942a9d1e1d72782
J Ben Anderson <revovisions@gmail.com>
2021-08-15 17:00:05 -0400

    finished login.js authentication with firebase

--- login.js
+++ login.js
@@ -18,4 +18,44 @@
   const login = document.getElementById("login");
   const signup = document.getElementById("signup");
   const logout = document.getElementById("logout");
+
+  //login
+  login.addEventListener("click", (e) => {
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.catch((e) => console.log(e.message));
+  });
+
+  //signup
+  signup.addEventListener("click", (e) => {
+    // TODO: check for real email
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+  });
+
+  //logout
+  logout.addEventListener("click", (e) => {
+    firebase.auth().signOut();
+  });
+
+  //login state
+  firebase.auth().onAuthStateChanged((firebaseUser) => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      logout.style.display = "inline";
+      login.style.display = "none";
+      signup.style.display = "none";
+    } else {
+      console.log("User is not logged in");
+      logout.style.display = "none";
+      login.style.display = "inline";
+      signup.style.display = "inline";
+    }
+  });
 })();
