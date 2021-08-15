c41147d4a575a921737abf88f0fee81253da78f8
J Ben Anderson <revovisions@gmail.com>
2021-08-15 13:13:02 -0400

    added sdk setup and configuration from project settings on firebase to database.js

---
+++ database.js
@@ -0,0 +1,16 @@
+(function () {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDENA8FqMVh94h-7r2_OlHZPts2b6yQdM8",
+    authDomain: "courso-7aca1.firebaseapp.com",
+    projectId: "courso-7aca1",
+    storageBucket: "courso-7aca1.appspot.com",
+    messagingSenderId: "9456444517",
+    appId: "1:9456444517:web:35c90b1f9ba30660ec528c",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // handle on firebase database
+  const db = firebase.database();
+})();
